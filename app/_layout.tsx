3ca5cf3fe7e9cf54cf21b9ceffcc26c11c742442
yaredow <yaredyilma11@gmail.com>
2026-02-19 19:05:44 +0300

    chore: properly configure tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
+import tamaguiConfig from "@/tamagui.config";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    if (!loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack />
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
+  const inset = useSafeAreaInsets();
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingTop: inset.top,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
