93760fa618c25aaec57284994fb602a0decf4eaf
yaredow <yaredyilma11@gmail.com>
2026-02-21 16:23:50 +0300

    feat: configure hugeicon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,8 +2,18 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => {},
+        }}
+      />
       <Tabs.Screen name="search" options={{ title: "Search" }} />
     </Tabs>
   );
